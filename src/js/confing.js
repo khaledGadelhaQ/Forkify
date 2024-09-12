0a289e59e7c12f261ca650f5b8ef72b8266c4885
khaled <khaledmogadelhaq@gmail.com>
2024-09-12 17:30:04 +0300

    Building the final files using parcel

--- src/js/confing.js
+++ src/js/confing.js
@@ -2,5 +2,5 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const TIMEOUT_SEC = 10;
 export const RECIPES_PER_PAGE = 10;
 export const FIRST_PAGE = 1;
-export const KEY = '435d968a-4bfc-43a8-bf21-a65ca8760edd';
+export const KEY = '447f5fea-1340-4409-939b-cf88cbf6aaee';
 export const CLOSE_FORM_TIME = 2.5;
